fb8cf531824b763474cefbf2fd90acb40bc834df
heidi1105 <heidi1105@gmail.com>
2024-03-13 11:51:42 -0600

    Update TripController.java

--- Java4MVC/packages/TripController.java
+++ Java4MVC/packages/TripController.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.heidichen.mvcdemo.models.Trip;
 import com.heidichen.mvcdemo.services.TripService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class TripController {
@@ -20,30 +25,66 @@ public class TripController {
 		this.tripService = tripService;
 	}
 	
+//	@Autowired
+//	private TripService tripService;
+	
 	@GetMapping("/trips")
-	public String displayTripDashboard(Model model) {
-		// 1. get the list of trip from service
+	public String displayDashboardPage(Model model) {
+		// 1. get the triplist from service
 		List<Trip> tripList = tripService.allTrips();
-		
-		// 2. store it in Model model for jsp to use
+		// 2. store the triplist in the Model model
 		model.addAttribute("tripList", tripList);
-		
 		return "tripDashboard.jsp";
 	}
 	
-	// Find one
+	
+	// Details : 1. Store the id from route
 	@GetMapping("/trips/{id}")
-	public String displayTripDetails(@PathVariable("id")Long id, Model model) {
-		// 1. get the id from PathVariable
-		// 2. get the Trip from service based on the id from pathvariable
+	public String displayDetailsPage(@PathVariable("id") Long id, Model model) {
+		// 2. get the trip from service with the id
 		Trip oneTrip = tripService.oneTrip(id);
 		// 3. store it in Model model
 		model.addAttribute("oneTrip", oneTrip);
-		
 		return "tripDetails.jsp";
 	}
+	
+	
+	// CREATE
+	// Render the form page
+	@GetMapping("/trips/new")
+	public String renderCreateForm(Model model) {
+		// @ModelAttribute("Book") Book book
+		// - create a empty Book and store it in Model model
+		Trip emptyTrip = new Trip();
+		model.addAttribute("newTrip", emptyTrip);
+		return "newTrip.jsp";
+	}
+	
+	
+	// Process the form
+	@PostMapping("/trips/new")
+	public String processCreateForm(
+			@Valid @ModelAttribute("newTrip") Trip newTrip, BindingResult result, Model model) {
+		// check if there is any error
+		if(result.hasErrors()) { // errors are bound into that ModelAttribute
+			return "newTrip.jsp";
+		}else {
+			tripService.createTrip(newTrip);
+			return "redirect:/trips";	
+		}
+	}
+	
+	// Edit
+	// 1. get the id from Path
+	// 2. get the trip from the service with the id
+	// 3. store it in Model model with the name as the form:form modelAttribute
+	// 4. jsp: SAME AS CREATE but update the action & method --> PUT
+	
 	
 	
 	
 	
 }
+
+
+
